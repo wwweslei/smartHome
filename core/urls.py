feaad644543858ebe8ceb7cf1a2b7644da465fc1
weslei <www.weslei@gmail.com>
2017-05-21 18:16:03 -0300

    criado aba tradução

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from core.views import home, luz, ventilador
+from core.views import home, luz, ventilador, tradutor
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^luz$', luz, name='luz'),
     url(r'^ventilador$', ventilador, name='ventilador'),
-
+    url(r'^tradutor$', tradutor, name='tradutor'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .rf433 import send, teste_usb
+from .tratutor import Traduzir
 
 
 def home(request):
@@ -17,3 +18,13 @@ def ventilador(request):
     con = teste_usb()
     send('v')
     return render(request, 'core/index.html', {"send": con})
+
+
+def tradutor(request):
+    if request.method == "POST":
+        frase = request.POST['texto']
+        traduzir = Traduzir.traduzir(frase)
+        return render(request, 'core/tradutor.html', {'tradução': traduzir})
+
+    else:
+        return render(request, 'core/tradutor.html')
